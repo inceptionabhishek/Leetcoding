cb57bcbbc7fbe29d48a8dd3d2c6abca3b9322315
Abhishek kumar <65409282+inceptionabhishek@users.noreply.github.com>
2022-10-25 19:37:54 +0530

    Time: 80 ms (41.02%), Space: 14 MB (55.99%) - LeetHub

---
+++ 1573-number-of-ways-to-split-a-string/1573-number-of-ways-to-split-a-string.cpp
@@ -0,0 +1,87 @@
+class Solution {
+public:
+    long long moduloMultiplication(long a,
+                            long b, long mod)
+    {
+         
+        // Initialize result
+        long res = 0; 
+ 
+        // Update a if it is more than
+        // or equal to mod
+        a %= mod;
+ 
+        while (b > 0)
+        {
+             
+            // If b is odd, add a with result
+            if ((b & 1) > 0)
+            {
+                res = (res + a) % mod;
+            }
+ 
+            // Here we assume that doing 2*a
+            // doesn't cause overflow
+            a = (2 * a) % mod;
+ 
+            b >>= 1; // b = b / 2
+        }
+        return res;
+    }
+    int numWays(string s) {
+        int modd=1000000007;
+        int cnt=0;
+        for(int i=0;i<s.length();i++){
+            cnt += (s[i]=='1');
+        }
+        if(cnt%3!=0){
+            return 0;
+        }
+        int div=cnt/3;
+        int index1=-1;
+        int index2=-1;
+        bool found=0;
+        int currcnt=0;
+        vector <pair<int,int>> v;
+        for(int i=0;i<s.length();i++){
+            if(found){
+                if(s[i]=='1'){
+                    currcnt++;
+                    if(currcnt==div){
+                        index2=i;
+                        found=0;
+                        currcnt=0;
+                        v.push_back(make_pair(index1,index2));
+                        index1=-1;
+                        index2=-1;
+                    }
+                }
+            }else{
+                if(s[i]=='1'){
+                    currcnt++;
+                    found=1;
+                    index1=i;
+                    index2=i;
+                    if(currcnt==div){
+                        index2=i;
+                        found=0;
+                        currcnt=0;
+                        v.push_back(make_pair(index1,index2));
+                        index1=-1;
+                        index2=-1;
+                    }
+                }
+            }
+        }
+        if(v.size()==0){
+            return ((s.length()-1)*(s.length()-2)/2)%modd;
+        }
+        for(auto j:v){
+            cout << j.first << ' ' << j.second << endl;
+        }
+        int cnt1=abs(v[0].second-v[1].first)%modd;
+        int cnt2=abs(v[1].second-v[2].first)%modd;
+        
+        return moduloMultiplication(cnt1,cnt2,modd);
+    }
+};
