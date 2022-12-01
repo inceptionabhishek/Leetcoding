5174dc26fd70e5c6acba58986aff6f7ef42959f6
Abhishek kumar <65409282+inceptionabhishek@users.noreply.github.com>
2022-12-01 10:48:53 +0530

    Time: 31 ms (5.65%), Space: 6.6 MB (83.41%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool chk(char a){
+        if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u'){
+            return true;
+        }
+        if(a=='A' || a=='E' || a=='I' || a=='O' || a=='U'){
+            return true;
+        }
+        return false;
+    }
+    bool halvesAreAlike(string s) {
+        int cnt1=0,cnt2=0;
+        for(int i=0;i<s.length()/2;i++){
+            if(chk(s[i])){
+                cnt1++;
+            }
+        }
+        for(int i=s.length()/2;i<s.length();i++){
+            cout << s[i] << ' ';
+            if(chk(s[i])){
+                cnt2++;
+            }
+        }
+        cout << cnt1 << ' ' << cnt2 << endl;
+        return cnt1==cnt2;
+    }
+};
