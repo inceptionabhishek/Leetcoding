d265ff94dbbcb314aa93430ab9fa8beb7b69b4d2
Abhishek kumar <65409282+inceptionabhishek@users.noreply.github.com>
2022-12-20 17:06:25 +0530

    Time: 29 ms (12.85%), Space: 10.4 MB (80.63%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void dfs(vector<bool>&vis,int node,vector<vector<int>> &rooms){
+        if(vis[node]==true){
+            return;
+        }
+        vis[node]=true;
+        for(int i=0;i<rooms[node].size();i++){
+            dfs(vis,rooms[node][i],rooms);
+        }
+    }
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n=rooms.size();
+        vector<bool>vis(n+1,false);
+        vector<int> node = rooms[0];
+        vis[0]=true;
+        for(int i=0;i<node.size();i++){
+            dfs(vis,node[i],rooms);
+        }
+        for(int i=0;i<vis.size();i++){
+            cout << vis[i] << ' ';
+        }
+        cout << endl;
+        for(int i=0;i<vis.size()-1;i++){
+            
+            if(vis[i]==false){
+                return false;
+            }
+        }
+        return true;
+    }
+};
