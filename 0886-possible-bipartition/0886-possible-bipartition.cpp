330723e2f42c14f072ceb5611ebe15c9ac1eb916
Abhishek kumar <65409282+inceptionabhishek@users.noreply.github.com>
2022-12-21 13:54:31 +0530

    Time: 227 ms (87.83%), Space: 63.9 MB (92.03%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        vector<int> adj[n+1];
+        vector<int> vis(n+1,0);
+        for(int i=0;i<dislikes.size();i++){
+            int u=dislikes[i][0];
+            int v=dislikes[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        queue<int> q;
+        q.push(1);
+        vector<int>color(n+1,0);
+        
+        
+        for(int i=1;i<=n;i++){
+            if(vis[i]==0){
+                color[i]=1;
+                vis[i]=1;
+                q.push(i);
+                while(!q.empty()){
+                    int fi=q.front();
+                    vis[fi]=1;
+                    q.pop();
+                    for(auto j:adj[fi]){
+                        if(color[j]==0){
+                            color[j]=3-color[fi];
+                            q.push(j);
+                        }else if(color[j]==color[fi]){
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0;i<vis.size();i++){
+            cout << vis[i] << ' ';
+        }
+        cout << endl;
+        for(int i=0;i<color.size();i++){
+            cout << color[i] << ' ';
+        }
+        cout << endl;
+        return true;
+    }
+};
