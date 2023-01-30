79a89de25e099dc71e175648f487682728d6c0a2
Abhishek kumar <65409282+inceptionabhishek@users.noreply.github.com>
2023-01-30 09:26:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.21%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dp[40];
+    int fibo(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n<=2){
+            return 1;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=fibo(n-1)+fibo(n-2)+fibo(n-3);
+    }
+    int tribonacci(int n) {
+        memset(dp,-1,sizeof(dp));
+        return fibo(n);
+    }
+};
