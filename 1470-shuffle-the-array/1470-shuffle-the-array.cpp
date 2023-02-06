44f3e78540841ef1484b0b08a3c44d466bde4aeb
Abhishek kumar <65409282+inceptionabhishek@users.noreply.github.com>
2023-02-06 13:27:31 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (31.98%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        
-        int index = 0;
-        vector<int> res(nums.size());
-
-        for(int i = 0; i < nums.size(); i++) {
-
-            res[i] = i % 2 == 0 ? nums[index] : nums[index + n];
-            if(i % 2) index++;
+    vector<int> res;
+        for(int i=0;i<n;i++)
+        {
+            res.push_back(nums[i]);
+            res.push_back(nums[n+i]);
         }
-
         return res;
     }
 };
