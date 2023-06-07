0ab8861d2b5cd596a14fc337bee7d75b961d3add
Abhishek kumar <65409282+inceptionabhishek@users.noreply.github.com>
2023-06-07 17:20:52 +0530

    Time: 7 ms (40.05%), Space: 6.2 MB (16.51%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string decToBinary(int n)
+{
+        string s="";
+    // Size of an integer is assumed to be 32 bits
+    for (int i = 31; i >= 0; i--) {
+        int k = n >> i;
+        if (k & 1)
+            s+='1';
+        else
+            s+='0';
+    }
+        return s;
+}
+ 
+    int minFlips(int a, int b, int c) {
+        string a1=decToBinary(a);
+        string a2=decToBinary(b);
+        string a3=decToBinary(c);
+        cout << a1 << endl;
+        cout << a2 << endl;
+        cout << a3 << endl;
+        int cnt=0;
+        for(int i=0;i<=31;i++){
+            if(a3[i]=='1'){
+                cout << a1[i] << ' ' << a2[i] << endl;
+                if(a1[i]=='0' && a2[i]=='0'){
+                    cnt++;
+                }
+            }else{
+                
+                if(a1[i]=='1' && a2[i]=='1'){
+                    cout << "dd" << endl;
+                    cnt+=2;
+                }else if(a1[i]=='1' || a2[i]=='1'){
+                    cnt+=1;
+                }
+            }
+        }
+        return cnt;
+    }
+};
