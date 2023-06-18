84793e16dae7f8a681dbd4269b0de1b725b5684b
Abhishek kumar <65409282+inceptionabhishek@users.noreply.github.com>
2023-06-18 11:03:44 +0530

    Time: 475 ms (87.50%), Space: 19 MB (37.50%) - LeetHub

---
+++ 2741-special-permutations/2741-special-permutations.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int dp[15][17000];
+    int mod=1000000007;
+    int rec(int previndex,vector<int>&nums,int mask){
+        //cout << previndex << ' ' << mask << endl;
+        bool ok=0;
+        for(int i=0;i<nums.size();i++){
+            if(!(mask&(1<<i))){
+                ok=1;
+            }
+        }
+        if(!ok){
+            return 1;
+        }
+        if(dp[previndex][mask]!=-1){
+            return dp[previndex][mask]%mod;
+        }
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(!(mask&(1<<i))){
+                if(nums[i]%nums[previndex]==0 || nums[previndex]%nums[i]==0){
+                    int newmask=mask;
+                    newmask |= (1<<i);
+                    int curr = rec(i,nums,newmask)%mod;
+                    //cout << "curr : " <<curr << endl;
+                    ans = (ans+curr)%mod;
+                    ans = ans % mod;
+                }
+            }
+        }
+        return dp[previndex][mask]=ans%mod;
+    }
+    int specialPerm(vector<int>& nums) {
+        int ans=0;
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<nums.size();i++){
+            int mask = 0;
+            mask |= (1 << i);
+            int val = rec(i,nums,mask);
+            ans = (ans+val)%mod;
+        }
+        return ans;
+    }
+};
