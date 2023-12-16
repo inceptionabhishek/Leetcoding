71b05b19b39032a8a4d4bb376768d8fcf5eb42f3
Abhishek kumar <65409282+inceptionabhishek@users.noreply.github.com>
2023-12-16 09:38:22 +0530

    Time: 3 ms (97.78%), Space: 7.6 MB (98.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return t==s;
+        vector<int>p(26,0);
+        for(int i=0;i<s.length();i++){
+            p[s[i]-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            p[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(p[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
